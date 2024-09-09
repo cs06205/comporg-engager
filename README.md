a5a96629df64f2f471cb212b953a41cd000a8965
Nick Ivanov <sm230@pm.me>
2024-09-09 14:49:36 -0400

    Version 0.3

--- README.md
+++ README.md
@@ -1,20 +1,35 @@
 # CompOrg Engager - a small tool that brings classroom engagement to the next level
 
-Copyright (C) 2023 Dr. Nick Ivanov @ Rowan University
+Copyright (C) 2023,2024 Dr. Nick Ivanov @ Rowan University
 
 e-mail: ivanov@rowan.edu, LICENSE: GNU GPL 3
 
-Version: 0.2
+Version: 0.3
 
 ## Usage
 
-1. Add students to a JSON file (prefixed with `class-`) using `sample.json` as an example; mark the attendance (e.g., `class-cs06205-1.json`).
+The program can operate in two modes: I) Lucky Student Mode and II) Group Forming Mode
+
+### I. Lucky Student Mode
+
+This mode allows to pick a random attending student.
+
+1. Add students to a JSON file using `sample.json` as an example; mark the attendance (e.g., `class-cs06205-1.json`).
 2. Run this command:
 ```
 cargo run class-cs06205-1.json
 ```
-3. Put the Lucky One on the spot by asking question, asking to write something on the board, etc.
-4. Give the Lucky One a participation bonus point.
+
+### II. Group Forming Mode
+
+This mode is used to make groups of desired size randomly from the attending students. The group captain in each group is chosen randomly.
+
+1. Add students to a JSON file using `sample.json` as an example; mark the attendance (e.g., `class-cs06205-1.json`).
+2. Run this command:
+```
+cargo run groups <N> class-cs06205-1.json
+```
+Where N is an integer number between 2 and 32 indicating the desired group size. The algorithm will create maximum number of groups of size N, except possibly for the last group, which will be filled with the remaining students. If the there is only one remaining student, they will be added to the last group, effectively making its size N+1.
 
 ## Frequently Asked Questions
 
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use rand::Rng;
 use std::env;
 use std::process;
 use rand::seq::SliceRandom;
+use std::str::FromStr;
 
 fn main() {
     if env::args().len() == 2 {
@@ -31,54 +32,65 @@ fn main() {
             println!("UNEXPECTED BEHAVIOR: Please tell the developer that Bug 0d7c3528509aeacb occurred.");
             process::exit(2);
         }
-    } else if env::args().len() == 3 {
+    } else if env::args().len() == 4 {
         if let Some(arg) = env::args().nth(1) {
             if arg == "groups" {
                 if let Some(arg) = env::args().nth(2) {
-                    let data = fs::read_to_string(arg)
-                    .expect("Unable to read file.");
+                    let target_group_size = u8::from_str(&arg)
+                    .expect("The target group size should be an integer number between 2 and 32.");
 
-                    let json: serde_json::Value = serde_json::from_str(&data)
-                    .expect("Json error.");
-
-                    let mut students_present: Vec<String> = Vec::new();
-
-                    if let Some(array) = json.as_array() {
-                        for student in array {
-                            if student["present"] == true {
-                                students_present.push(student["name"].to_string().trim_matches('\"').to_string());
-
+                    if target_group_size < 2 || target_group_size > 32 {
+                        println!("The target group size should be an integer number between 2 and 32.");
+                        process::exit(2);
+                    }
+                    
+                    if let Some(arg) = env::args().nth(3) {
+                        let data = fs::read_to_string(arg)
+                        .expect("Unable to read file.");
+    
+                        let json: serde_json::Value = serde_json::from_str(&data)
+                        .expect("Json error.");
+    
+                        let mut students_present: Vec<String> = Vec::new();
+    
+                        if let Some(array) = json.as_array() {
+                            for student in array {
+                                if student["present"] == true {
+                                    students_present.push(student["name"].to_string().trim_matches('\"').to_string());
+    
+                                }
                             }
                         }
-                    }
-
-                    students_present.shuffle(&mut rand::thread_rng());
-
-                    
-                    let (mut counter, mut group_counter) = (0, 1);
-                    
-                    let n = students_present.len();
-
-                    for student in students_present {
-                        let mut captain = false;
-                        if (n-counter) >= 2 && counter % 3 == 0 {
-                            println!("\nGROUP {}:", group_counter);
-                            group_counter += 1;
-                            captain = true;
-                        }
-
-                        if captain == true {
-                            println!("{} <== GROUP CAPTAIN", student);
-                        } else {
-                            println!("{}", student);
-                        }
+    
+                        students_present.shuffle(&mut rand::thread_rng());
+    
+                        let (mut counter, mut group_counter) = (0, 1);
                         
-                        counter += 1;
+                        let n = students_present.len();
+    
+                        for student in students_present {
+                            let mut captain = false;
+                            if (n-counter) >= 2 && counter % target_group_size as usize == 0 {
+                                println!("\nGROUP {}:", group_counter);
+                                group_counter += 1;
+                                captain = true;
+                            }
+    
+                            if captain == true {
+                                println!("{} <== GROUP CAPTAIN", student);
+                            } else {
+                                println!("{}", student);
+                            }
+                            
+                            counter += 1;
+                        }
+                    } else {
+                        println!("UNEXPECTED BEHAVIOR: Please tell the developer that Bug bb882e3d1bee97da occurred.");
+                        process::exit(2);
                     }
-                } else {
-                    println!("UNEXPECTED BEHAVIOR: Please tell the developer that Bug bb882e3d1bee97da occurred.");
-                    process::exit(2);
                 }
+
+                
             } else {
                 println!("USAGE: comporg-engager [groups] <CLASS_JSON_FILE_PATH>");
                 process::exit(1);
